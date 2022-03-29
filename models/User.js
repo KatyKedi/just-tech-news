a361ab5c90791509997daae69bc293e449b55d55
KatyKedi <katykat5bros@gmail.com>
2022-03-29 11:54:39 -0700

    post init

--- models/User.js
+++ models/User.js
@@ -69,12 +69,10 @@ User.init(
             },
             // set up beforeUpdate lifecycle "hook" functionality
             async beforeUpdate(updatedUserData) {
-              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-              return updatedUserData;
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
             }
-        }
-    },
-    {
+        },
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
